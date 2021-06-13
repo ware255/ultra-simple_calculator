ac8ad4ae7dc65779bd75030a467fc4ab64c09bf5
ware255 <74850252+ware255@users.noreply.github.com>
2021-06-13 17:30:50 +0900

    Create calculator.f90

---
+++ calculator.f90
@@ -0,0 +1,107 @@
+subroutine tasizan()
+    real(8) :: x = 0
+    real(8) :: y = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, x
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, y
+    print*, '\n答え'
+    print*, x + y
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine tasizan
+
+subroutine hikizan()
+    real(8) :: x = 0
+    real(8) :: y = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, x
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, y
+    print*, '\n答え'
+    print*, x - y
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine hikizan
+
+subroutine kakezan()
+    real(8) :: x = 0
+    real(8) :: y = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, x
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, y
+    print*, '\n答え'
+    print*, x * y
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine kakezan
+
+subroutine warizan()
+    real(8) :: x = 0
+    real(8) :: y = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, x
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, y
+    print*, '\n答え'
+    print*, x / y
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine warizan
+
+subroutine heihoukon()
+    real(8) :: x = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, x
+    print*, '\n答え'
+    print*, sqrt(x)
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine heihoukon
+
+program calculator
+    integer :: i = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    do
+        print*, '\n------------------------'
+        write (*,fmt='(a)', advance='no') '何の計算をしますか？\n'
+        print*, '1 足し算'
+        print*, '2 引き算'
+        print*, '3 掛け算'
+        print*, '4 割り算'
+        print*, '5 平方根\n'
+        print*, '99 終了'
+        print*, '\n------------------------'
+        read *, i
+        select case(i)
+        case (1)
+            call tasizan()
+        case (2)
+            call hikizan()
+        case (3)
+            call kakezan()
+        case (4)
+            call warizan()
+        case (5)
+            call heihoukon()
+        case (99)
+            write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+            exit
+        case default
+            print*, 'そんなもんねぇよｗ'
+            read *
+            write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+        end select
+    end do
+end program calculator
