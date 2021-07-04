6bfe0dbd335de88b777e989a1f4553f0fea17fcb
ware255 <74850252+ware255@users.noreply.github.com>
2021-07-04 16:58:02 +0900

    Update calculator.f90

--- calculator.f90
+++ calculator.f90
@@ -65,7 +65,7 @@ end subroutine warizan
 subroutine heihoukon()
     implicit none
     real(kind=16) x
-    integer(16) i
+    integer(kind=8) i
     integer v1, v2
     v1 = 2;v2 = 3
     write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
@@ -98,11 +98,41 @@ subroutine ensyuritu()
     write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
 end subroutine ensyuritu
 
+subroutine syutyou()
+    implicit none
+    real(kind=16), parameter :: pi = 3.1415926535897932384626433832795028
+    real(kind=16) r
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '値を入力してください。\n'
+    read *, r
+    print*, '\n答え'
+    print*, 2*pi*r
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine syutyou
+
+subroutine nizyou()
+    implicit none
+    real(kind=16) :: x = 0
+    real(kind=16) :: y = 0
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+    write (*,fmt='(a)', advance='no') '二乗する値を入力してください。\n'
+    read *, x
+    write (*,fmt='(a)', advance='no') 'n乗する値を入力してください。\n'
+    read *, y
+    print*, '\n答え'
+    print*, x ** y
+    print*, '\nEnterを押してください。'
+    read *
+    write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
+end subroutine nizyou
+
 program calculator
     use, intrinsic :: iso_fortran_env
     implicit none
+    integer(kind=8) :: n = 0
     real(kind=16) x
-    character(256) i
     real(real128), parameter :: PI = 3.14159265358979323846264338327950288_real128
     write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
     do
@@ -115,11 +145,13 @@ program calculator
         print*, '5 平方根'
         print*, '6 円周率(π)'
         print*, '7 円の面積(πr^2)'
+        print*, '8 円の周長(2πr)'
+        print*, '9 べき乗'
         print*, '99 終了'
         print*, '\n------------------------'
-        read (*, '(a)') i
-        x = func_henkan(i)
-        select case(int(x))
+        read *, x
+        n = int(x)
+        select case(n)
         case (1)
             call tasizan()
         case (2)
@@ -139,21 +171,17 @@ program calculator
             write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
         case (7)
             call ensyuritu()
+        case (8)
+            call syutyou()
+        case (9)
+            call nizyou()
         case (99)
             write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
             exit
         case default
             print*, 'そんなもんねぇよｗ'
-            read *
+            call sleep(1)
             write (*,fmt='(a)', advance='no') '\x1b[2J\x1b[3J\x1b[H'
         end select
     end do
-    contains
-    function func_henkan(i)
-        implicit none
-        real(kind=16) n, func_henkan
-        character(len=256) i
-        read (i, *) n
-        func_henkan = n
-    end function
 end program calculator
